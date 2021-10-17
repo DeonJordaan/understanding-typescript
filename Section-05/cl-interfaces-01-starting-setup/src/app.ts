fb4166210cba985d3852308f08d33ec8a9adfe5a
DeonJ76 <deon.jordaan76@gmail.com>
2021-10-17 10:52:48 +0200

    Section 5 complete - Classes & Interfaces

--- Section-05/cl-interfaces-01-starting-setup/src/app.ts
+++ Section-05/cl-interfaces-01-starting-setup/src/app.ts
@@ -1,18 +1,42 @@
-interface Person {
-	name: string;
-	age: number;
+interface Named {
+	readonly name?: string;
+	outputName?: string;
+}
 
+//Interface Greetable inheriting from Named
+interface Greetable extends Named {
 	greet(phrase: string): void;
 }
 
-let user1: Person;
+class Person implements Greetable {
+	name?: string;
+	age = 45;
 
-user1 = {
-	name: 'Max',
-	age: 30,
+	constructor(n?: string) {
+		if (n) {
+			this.name = n;
+		}
+	}
+
 	greet(phrase: string) {
-		console.log(phrase + ' ' + this.name);
-	},
-};
+		if (this.name) {
+			console.log(`${phrase} ${this.name}`);
+		} else {
+			console.log('Hi!');
+		}
+	}
+}
+
+let user1: Greetable;
+
+user1 = new Person('Nadia');
 
 user1.greet('Hi there - I am');
+
+//Using interfaces to declare a Function Type
+interface AddFn {
+	(a: number, b: number): number;
+}
+
+//Mark methods as optional with>>>
+//optional! => mymethod?(){}
