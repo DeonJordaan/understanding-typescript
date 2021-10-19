b2ee0772569d8d812fb89dbff246b0f44818d264
DeonJ76 <deon.jordaan76@gmail.com>
2021-10-19 22:15:31 +0200

    Section 6 complete - Advanced Types

--- Section-06/adv-types-01-starting-setup/src/app.ts
+++ Section-06/adv-types-01-starting-setup/src/app.ts
@@ -21,6 +21,10 @@ type Numeric = number | boolean;
 
 type Universal = Combinable & Numeric;
 
+function add(a: number, b: string): string;
+function add(a: string, b: number): string;
+function add(a: string, b: string): string;
+function add(a: number, b: number): number;
 function add(a: Combinable, b: Combinable) {
 	if (typeof a === 'string' || typeof b === 'string') {
 		return a.toString() + b.toString();
@@ -28,40 +32,42 @@ function add(a: Combinable, b: Combinable) {
 	return a + b;
 }
 
-type UnknownEmployee = Employee | Admin;
+const result = add(1, 'Kat');
 
-function printEmployeeInformation(emp: UnknownEmployee) {
-	console.log('Name: ' + emp.name);
-	if ('privileges' in emp) {
-		console.log('Privileges: ' + emp.privileges);
-	}
-	if ('startDate' in emp) {
-		console.log('Start Date: ' + emp.startDate);
-	}
-}
+// type UnknownEmployee = Employee | Admin;
 
-printEmployeeInformation(e1);
+// function printEmployeeInformation(emp: UnknownEmployee) {
+// 	console.log('Name: ' + emp.name);
+// 	if ('privileges' in emp) {
+// 		console.log('Privileges: ' + emp.privileges);
+// 	}
+// 	if ('startDate' in emp) {
+// 		console.log('Start Date: ' + emp.startDate);
+// 	}
+// }
 
-class Car {
-	drive() {
-		console.log('Driving...');
-	}
-}
+// printEmployeeInformation(e1);
 
-class Truck {
-	drive() {
-		console.log('Truck DRIVING!!!');
-	}
+// class Car {
+// 	drive() {
+// 		console.log('Driving...');
+// 	}
+// }
 
-	loadCargo(amount: number) {
-		console.log('Loading cargo...' + amount);
-	}
-}
+// class Truck {
+// 	drive() {
+// 		console.log('Truck DRIVING!!!');
+// 	}
 
-type Vehicle = Car | Truck;
+// 	loadCargo(amount: number) {
+// 		console.log('Loading cargo...' + amount);
+// 	}
+// }
 
-const v1 = new Car();
-const v2 = new Truck();
+// type Vehicle = Car | Truck;
+
+// const v1 = new Car();
+// const v2 = new Truck();
 
 // Simple solution
 // function useVehicle(vehicle: Vehicle) {
@@ -71,41 +77,41 @@ const v2 = new Truck();
 // 	}
 // }
 // Better solution
-function useVehicle(vehicle: Vehicle) {
-	vehicle.drive();
-	if (vehicle instanceof Truck) {
-		vehicle.loadCargo(1000);
-	}
-}
+// function useVehicle(vehicle: Vehicle) {
+// 	vehicle.drive();
+// 	if (vehicle instanceof Truck) {
+// 		vehicle.loadCargo(1000);
+// 	}
+// }
 
-useVehicle(v1);
-useVehicle(v2);
+// useVehicle(v1);
+// useVehicle(v2);
 
-interface Bird {
-	type: 'bird';
-	flyingSpeed: number;
-}
+// interface Bird {
+// 	type: 'bird';
+// 	flyingSpeed: number;
+// }
 
-interface Horse {
-	type: 'horse';
-	runningSpeed: number;
-}
+// interface Horse {
+// 	type: 'horse';
+// 	runningSpeed: number;
+// }
 
-type Animal = Bird | Horse;
+// type Animal = Bird | Horse;
 
-function moveAnimal(animal: Animal) {
-	let speed;
-	switch (animal.type) {
-		case 'bird':
-			speed = animal.flyingSpeed;
-			break;
-		case 'horse':
-			speed = animal.runningSpeed;
-	}
-	console.log('Moving with speed: ' + speed);
-}
+// function moveAnimal(animal: Animal) {
+// 	let speed;
+// 	switch (animal.type) {
+// 		case 'bird':
+// 			speed = animal.flyingSpeed;
+// 			break;
+// 		case 'horse':
+// 			speed = animal.runningSpeed;
+// 	}
+// 	console.log('Moving with speed: ' + speed);
+// }
 
-moveAnimal({ type: 'bird', flyingSpeed: 10 });
+// moveAnimal({ type: 'bird', flyingSpeed: 10 });
 
 // Type Casting
 // Option 1
@@ -114,14 +120,23 @@ moveAnimal({ type: 'bird', flyingSpeed: 10 });
 // );
 
 // Option 2
-const userInputElement = document.getElementById(
-	'element-id'
-) as HTMLInputElement;
+// const userInputElement = document.getElementById(
+// 	'element-id'
+// ) as HTMLInputElement;
 
-userInputElement.value = 'Type Casting!';
+// userInputElement.value = 'Type Casting!';
 
-// With an if-check
-if (userInputElement) {
-	(userInputElement as HTMLInputElement).value =
-		'Type Casting with an if-checl';
-}
+// // With an if-check
+// if (userInputElement) {
+// 	(userInputElement as HTMLInputElement).value =
+// 		'Type Casting with an if-checl';
+// }
+
+// interface ErrorContainer {
+// 	[prop: string]: string;
+// }
+
+// const errorBag: ErrorContainer = {
+// 	email: 'Not a valid email',
+// 	username: 'Must start with a capital letter',
+// };
