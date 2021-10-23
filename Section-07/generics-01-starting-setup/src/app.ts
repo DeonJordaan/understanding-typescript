46667cde03482751c39655b64ed3cfb1ae8798c9
DeonJ76 <deon.jordaan76@gmail.com>
2021-10-23 22:14:50 +0200

    Section 7 complete - Generics

--- Section-07/generics-01-starting-setup/src/app.ts
+++ Section-07/generics-01-starting-setup/src/app.ts
@@ -13,55 +13,87 @@ function merge<T, U>(objA: T, objB: U) {
 	return Object.assign(objA, objB);
 }
 
-console.log(merge({ name: 'Deon' }, { age: 45 }));
+// console.log(merge({ name: 'Deon' }, { age: 45 }));
 
-const mergedObj = merge({ name: 'Nadia', hobbies: ['Yoga'] }, { age: 40 });
-console.log(mergedObj.age);
+// const mergedObj = merge({ name: 'Nadia', hobbies: ['Yoga'] }, { age: 40 });
+// console.log(mergedObj.age);
 
-interface Lengthy {
-	length: number;
+// interface Lengthy {
+// 	length: number;
+// }
+
+// function countAndDescribe<T extends Lengthy>(element: T): [T, string] {
+// 	let descriptionText = 'Got no value';
+// 	if (element.length === 1) {
+// 		descriptionText = 'Got 1 element';
+// 	} else if (element.length > 1) {
+// 		descriptionText = `Got ${element.length} elements`;
+// 	}
+// 	return [element, descriptionText];
+// }
+
+// console.log(countAndDescribe('Hoe lyk dit nou'));
+
+// function extractAndConvert<T extends object, U extends keyof T>(
+// 	obj: T,
+// 	key: U
+// ) {
+// 	return 'Value: ' + obj[key];
+// }
+
+// extractAndConvert({ name: 'Frikkie' }, 'name');
+
+// class DataStorage<T> {
+// 	private data: T[] = [];
+
+// 	addItem(item: T) {
+// 		this.data.push(item);
+// 	}
+
+// 	removeItem(item: T) {
+// 		this.data.splice(this.data.indexOf(item), 1);
+// 	}
+
+// 	getItems() {
+// 		return [...this.data];
+// 	}
+// }
+
+// const textStorage = new DataStorage<string>();
+// textStorage.addItem('Heksie');
+
+// const numberStorage = new DataStorage<number>();
+
+// const objStorage = new DataStorage<object>();
+
+//NOTE Partial Types
+interface CourseGoal {
+	title: string;
+	description: string;
+	completeUntil: Date;
 }
 
-function countAndDescribe<T extends Lengthy>(element: T): [T, string] {
-	let descriptionText = 'Got no value';
-	if (element.length === 1) {
-		descriptionText = 'Got 1 element';
-	} else if (element.length > 1) {
-		descriptionText = `Got ${element.length} elements`;
-	}
-	return [element, descriptionText];
+// The usual setup procedure
+// function createCourseGoal(
+// 	title: string,
+// 	description: string,
+// 	date: Date
+// ): CourseGoal {
+// 	return { title: title, description: description, completeUntil: date };
+// }
+
+//The alternate procedure wqhich doen't work initially
+function createCourseGoal(
+	title: string,
+	description: string,
+	date: Date
+): CourseGoal {
+	let courseGoal: Partial<CourseGoal> = {};
+	courseGoal.title = title;
+	courseGoal.description = description;
+	courseGoal.completeUntil = date;
+	return courseGoal as CourseGoal;
 }
 
-console.log(countAndDescribe('Hoe lyk dit nou'));
-
-function extractAndConvert<T extends object, U extends keyof T>(
-	obj: T,
-	key: U
-) {
-	return 'Value: ' + obj[key];
-}
-
-extractAndConvert({ name: 'Frikkie' }, 'name');
-
-class DataStorage<T> {
-	private data: T[] = [];
-
-	addItem(item: T) {
-		this.data.push(item);
-	}
-
-	removeItem(item: T) {
-		this.data.splice(this.data.indexOf(item), 1);
-	}
-
-	getItems() {
-		return [...this.data];
-	}
-}
-
-const textStorage = new DataStorage<string>();
-textStorage.addItem('Heksie');
-
-const numberStorage = new DataStorage<number>();
-
-const objStorage = new DataStorage<object>();
+//NOTE Readonly Types
+const names: Readonly<string[]> = ['Lavinia', 'Heksie'];
