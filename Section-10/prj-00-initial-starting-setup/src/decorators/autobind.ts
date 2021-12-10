5efd27d6885a7d93eb8bd486a4f3ce838c88efbb
DeonJ76 <deon.jordaan76@gmail.com>
2021-12-10 22:34:55 +0200

    Section 10 - Modules & Namespaces - Namespaces

---
+++ Section-10/prj-00-initial-starting-setup/src/decorators/autobind.ts
@@ -0,0 +1,18 @@
+namespace App {
+	// NOTE autobind Decorator
+	export function autobind(
+		_: any,
+		_2: string,
+		descriptor: PropertyDescriptor
+	) {
+		const originalMethod = descriptor.value;
+		const adjustedDescriptor: PropertyDescriptor = {
+			configurable: true,
+			get() {
+				const boundFunction = originalMethod.bind(this);
+				return boundFunction;
+			},
+		};
+		return adjustedDescriptor;
+	}
+}
