b67b6618f335a8c94a6f7880f6b4553162a73175
DeonJ76 <deon.jordaan76@gmail.com>
2021-12-11 18:39:53 +0200

    Section 10 complete - Modules & Namespaces - ES6 Modules

---
+++ Section-10/es6-modules/src/decorators/autobind.ts
@@ -0,0 +1,12 @@
+// NOTE autobind Decorator
+export function autobind(_: any, _2: string, descriptor: PropertyDescriptor) {
+	const originalMethod = descriptor.value;
+	const adjustedDescriptor: PropertyDescriptor = {
+		configurable: true,
+		get() {
+			const boundFunction = originalMethod.bind(this);
+			return boundFunction;
+		},
+	};
+	return adjustedDescriptor;
+}
